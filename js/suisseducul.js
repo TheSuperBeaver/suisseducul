b8fd0f3720cd6a2f7a0ea344685f25c25f855550
Matthieu <mestrez.matthieu@gmail.com>
2020-05-25 02:21:37 +0200

    style

--- js/suisseducul.js
+++ js/suisseducul.js
@@ -5,99 +5,88 @@ var quands = []
 var debuts = []
 var verbes = []
 
+var debut;
+var quand;
+var personne1;
+var personne2;
+var personne3;
+var verbe;
+var objet;
+var lieu;
+
 function loadData() {
-	
 	var urlPersonnes = "https://spreadsheets.google.com/feeds/list/1-Z1JsVYIIudA6aWKqfz1GYhwSrRfvTnGmCj1H3wgdOs/1/public/values?alt=json";
 	var urlLieux = "https://spreadsheets.google.com/feeds/list/1-Z1JsVYIIudA6aWKqfz1GYhwSrRfvTnGmCj1H3wgdOs/2/public/values?alt=json";
 	var urlObjets = "https://spreadsheets.google.com/feeds/list/1-Z1JsVYIIudA6aWKqfz1GYhwSrRfvTnGmCj1H3wgdOs/3/public/values?alt=json";
 	var urlQuand = "https://spreadsheets.google.com/feeds/list/1-Z1JsVYIIudA6aWKqfz1GYhwSrRfvTnGmCj1H3wgdOs/4/public/values?alt=json";
 	var urlDebut = "https://spreadsheets.google.com/feeds/list/1-Z1JsVYIIudA6aWKqfz1GYhwSrRfvTnGmCj1H3wgdOs/5/public/values?alt=json";
 	var urlVerbe = "https://spreadsheets.google.com/feeds/list/1-Z1JsVYIIudA6aWKqfz1GYhwSrRfvTnGmCj1H3wgdOs/6/public/values?alt=json";
-                      
-	$.getJSON(urlPersonnes, function(data) {						  
-        var sheetData = data.feed.entry;
-        var i;
-        for (i = 0; i < sheetData.length; i++) {
-          var name = data.feed.entry[i]['gsx$personne']['$t'];		  
-		  personnes.push(name);
-        }
-      });
-	$.getJSON(urlLieux, function(data) {						  
-        var sheetData = data.feed.entry;
-		var i;
-        for (i = 0; i < sheetData.length; i++) {
-          var lieu = data.feed.entry[i]['gsx$lieux']['$t'];		  
-		  lieux.push(lieu);
-        }
-      });
-	$.getJSON(urlObjets, function(data) {						  
-        var sheetData = data.feed.entry;
-		var i;
-        for (i = 0; i < sheetData.length; i++) {
-          var objet = data.feed.entry[i]['gsx$objet']['$t'];		  
-		  objets.push(objet);
-        }
-      });
-	$.getJSON(urlQuand, function(data) {						  
-        var sheetData = data.feed.entry;
-		var i;
-        for (i = 0; i < sheetData.length; i++) {
-          var nom = data.feed.entry[i]['gsx$quand']['$t'];		  
-		  quands.push(nom);
-        }
-      });
-	$.getJSON(urlDebut, function(data) {						  
-        var sheetData = data.feed.entry;
-		var i;
-        for (i = 0; i < sheetData.length; i++) {
-          var nom = data.feed.entry[i]['gsx$debut']['$t'];		  
-		  debuts.push(nom);
-        }
-      });
-	$.getJSON(urlVerbe, function(data) {						  
-        var sheetData = data.feed.entry;
-		var i;
-        for (i = 0; i < sheetData.length; i++) {
-          var nom = data.feed.entry[i]['gsx$verbe']['$t'];		  
-		  verbes.push(nom);
-        }
-      });
-}
-	  
-function getPersonne(elementId) {
-	$("#" + elementId).removeClass('d-none');
-	var random = personnes[Math.floor(Math.random() * personnes.length)];
-	document.getElementById(elementId).innerHTML=random;
-}
-	
-function getObjet() {
-	$("#objet").removeClass('d-none');
-	var random = objets[Math.floor(Math.random() * objets.length)];
-	document.getElementById("objet").innerHTML=random;
-}
-	
-function getLieu() {
-	$("#lieu").removeClass('d-none');
-	var random = lieux[Math.floor(Math.random() * lieux.length)];
-	document.getElementById("lieu").innerHTML=random;
+    
+	getJson(urlPersonnes, personnes, 'gsx$personne');
+	getJson(urlLieux, lieux, 'gsx$lieux');
+	getJson(urlObjets, objets, 'gsx$objet');
+	getJson(urlQuand, quands, 'gsx$quand');
+	getJson(urlDebut, debuts, 'gsx$debut');
+	getJson(urlVerbe, verbes, 'gsx$verbe');
 }
 
-function getQuand() {
-	$("#quand").removeClass('d-none');
-	var random = quands[Math.floor(Math.random() * quands.length)];
-	document.getElementById("quand").innerHTML=random;
+function getJson(url, list, fieldName) {
+  $.getJSON(url, function(data) {						  
+    var sheetData = data.feed.entry;
+	var i;
+	for (i = 0; i < sheetData.length; i++) {
+		var value = data.feed.entry[i][fieldName]['$t'];
+		list.push(value);
+	}
+  });
+}
+
+function randomDebut() {
+	debut = debuts[Math.floor(Math.random() * debuts.length)];
+}
+function randomQuand() {
+	quand = quands[Math.floor(Math.random() * quands.length)];
+}
+function randomPersonne1() {
+	personne1 = personnes[Math.floor(Math.random() * personnes.length)];
+}
+function randomPersonne2() {
+	personne2 = personnes[Math.floor(Math.random() * personnes.length)];
+}
+function randomPersonne3() {
+	personne3 = personnes[Math.floor(Math.random() * personnes.length)];
+}
+function randomVerbe() {
+	verbe = verbes[Math.floor(Math.random() * verbes.length)];
+}
+function randomObjet() {
+	objet = objets[Math.floor(Math.random() * objets.length)];
+}
+function randomLieu() {
+	lieu = lieux[Math.floor(Math.random() * lieux.length)];
+}
+function replacePhrase() {
+	phrase = debut + " " + quand + " " + lieu + " " + personne1 + " et " + personne2 + " " + verbe + " " + objet + " de " + personne3;
+	var textWrapper = document.querySelector('.ml11 .letters');
+	textWrapper.innerHTML = phrase;
+	
+}
+
+var showText = function (target, message, index, interval) {   
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    setTimeout(function () { showText(target, message, index, interval); }, interval);
+  }
 }
 
 function generePhrase() {
-	$("#phrase").removeClass('d-none');
-	var debut = debuts[Math.floor(Math.random() * debuts.length)];
-	var quand = quands[Math.floor(Math.random() * quands.length)];
-	var personne1 = personnes[Math.floor(Math.random() * personnes.length)];
-	var personne2 = personnes[Math.floor(Math.random() * personnes.length)];
-	var verbe = verbes[Math.floor(Math.random() * verbes.length)];
-	var objet = objets[Math.floor(Math.random() * objets.length)];
-	var personne3 = personnes[Math.floor(Math.random() * personnes.length)];
-	var lieu = lieux[Math.floor(Math.random() * lieux.length)];
-	
-	document.getElementById("phrase").innerHTML=debut + " " + quand + " " + lieu + " " + personne1 + " et " + personne2 + " " + verbe + " " + objet + " de " + personne3 + " ";
+	randomDebut();
+	randomQuand();
+	randomPersonne1();
+	randomPersonne2();
+	randomPersonne3();
+	randomVerbe();
+	randomObjet();
+	randomLieu();
+	replacePhrase();
 }
